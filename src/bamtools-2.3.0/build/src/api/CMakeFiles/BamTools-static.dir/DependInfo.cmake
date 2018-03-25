73e1b9dcca9c88119f615b0615471e74475869c7
Evan Biederstedt <evan.biederstedt@gmail.com>
2018-03-25 00:32:53 -0400

    includerlancet

---
+++ src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/BamAlignment.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/BamAlignment.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/BamMultiReader.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/BamMultiReader.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/BamReader.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/BamReader.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/BamWriter.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/BamWriter.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/SamHeader.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/SamHeader.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/SamProgram.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/SamProgram.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/SamProgramChain.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/SamProgramChain.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/SamReadGroup.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/SamReadGroup.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/SamReadGroupDictionary.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/SamReadGroupDictionary.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/SamSequence.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/SamSequence.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/SamSequenceDictionary.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/SamSequenceDictionary.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/bam/BamHeader_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamHeader_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/bam/BamMultiReader_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamMultiReader_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/bam/BamRandomAccessController_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamRandomAccessController_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/bam/BamReader_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamReader_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/bam/BamWriter_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/bam/BamWriter_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/index/BamIndexFactory_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/index/BamIndexFactory_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/index/BamStandardIndex_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/index/BamStandardIndex_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/index/BamToolsIndex_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/index/BamToolsIndex_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/BamDeviceFactory_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamDeviceFactory_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/BamFile_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamFile_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/BamFtp_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamFtp_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/BamHttp_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamHttp_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/BamPipe_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BamPipe_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/BgzfStream_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/BgzfStream_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/ByteArray_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/ByteArray_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/HostAddress_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/HostAddress_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/HostInfo_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/HostInfo_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/HttpHeader_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/HttpHeader_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/ILocalIODevice_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/ILocalIODevice_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/RollingBuffer_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/RollingBuffer_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/TcpSocketEngine_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/TcpSocketEngine_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/TcpSocketEngine_unix_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/TcpSocketEngine_unix_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/io/TcpSocket_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/io/TcpSocket_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/sam/SamFormatParser_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/sam/SamFormatParser_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/sam/SamFormatPrinter_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/sam/SamFormatPrinter_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/sam/SamHeaderValidator_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/sam/SamHeaderValidator_p.cpp.o"
+  "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/src/api/internal/utils/BamException_p.cpp" "/Users/ebiederstedt/RLancet/src/bamtools-2.3.0/build/src/api/CMakeFiles/BamTools-static.dir/internal/utils/BamException_p.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BAMTOOLS_API_LIBRARY"
+  "_FILE_OFFSET_BITS=64"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
